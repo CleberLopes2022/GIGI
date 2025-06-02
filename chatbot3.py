a9f34e0ff65f04c53fa35a6f73d3325b0eb69d2a
Cleber Lopes de Sousa <97367011+CleberLopes2022@users.noreply.github.com>
2025-06-01 21:26:20 -0300

    Update chatbot3.py

--- chatbot3.py
+++ chatbot3.py
@@ -109,7 +109,7 @@ with st.sidebar:
                         margin-bottom: 15px;" />
             <p><b>Sou a GIGI, sua assistente virtual!</b></p>
         </div>
-        <div style="text-align: center; margin-bottom: 15px; color: blue">
+        <div style="text-align: center; margin-bottom: 15px; color: lightblue">
             <p>
                 Meu objetivo é facilitar o seu dia! 
                 Posso responder dúvidas frequentes,fornecer informações úteis e muito mais — tudo com inteligência artificial.
